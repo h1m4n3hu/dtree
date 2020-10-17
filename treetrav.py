27f42acbfaf9b4728b01ccc93226fb0c1093c487
h1m4n3hu <himanshusoni2708@gmail.com>
2020-10-17 14:15:48 +0530

    the tree

---
+++ treetrav.py
@@ -0,0 +1,42 @@
+class Node:
+    def __init__(self,value):
+        self.value=value
+        self.left=None
+        self.right=None
+
+class Stack:
+    def __init__(self):
+        self.l=[]
+    def push(self,value):
+        self.l.append(value)
+    def pop(self):
+        self.l.pop()
+    def peek(self):
+        return self.l[-1]
+
+class Tree:
+    def __init__(self,root):
+        self.root=root
+
+    def pre_traversal(self,start,res):
+        if start:
+            res+=str(start.value)+" "
+            res=self.pre_traversal(start.left,res)
+            res=self.pre_traversal(start.right,res)
+        return res
+
+    def show(self):
+        return self.pre_traversal(self.root,"")
+
+    def inorder_iter(self):
+        pass
+
+t=Tree(Node(10))
+t.root.left=Node(3)
+t.root.right=Node(6)
+t.root.left.left=Node(2)
+t.root.left.right=Node(4)
+t.root.right.left=Node(5)
+t.root.right.right=Node(7)
+#print(t.show())     #10 3 2 4 6 5 7
+t.inorder_iter()
