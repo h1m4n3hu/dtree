d8e5b71e8696783571efeaf7dbb6c512962dd98c
h1m4n3hu <himanshusoni2708@gmail.com>
2020-10-18 09:42:19 +0530

    the tree

--- treetrav.py
+++ treetrav.py
@@ -22,7 +22,6 @@ class Tree:
 
     def inorder_iter(self):
         k=self.root
-        l=[]
         while True:
             if k:
                 l.append(k)
@@ -34,13 +33,40 @@ class Tree:
             else:
                 break
 
+    def preorder_iter(self):
+        l.append(self.root)
+        while len(l)>0:
+            node=l.pop()
+            print(node.value)
+            if node.right:
+                l.append(node.right)
+            if node.left:
+                l.append(node.left)
 
-t=Tree(Node(10))
-t.root.left=Node(3)
-t.root.right=Node(6)
-t.root.left.left=Node(2)
-t.root.left.right=Node(4)
-t.root.right.left=Node(5)
+    def postorder_iter(self):
+        s1=[]
+        s2=[]
+        s1.append(self.root)
+        while s1:
+            node=s1.pop()
+            s2.append(node)
+            if node.left:
+                s1.append(node.left)
+            if node.right:
+                s1.append(node.right)
+        while s2:
+            node=s2.pop()
+            print(node.value)
+
+    def max(self):
+
+
+t=Tree(Node(1))
+t.root.left=Node(2)
+t.root.right=Node(3)
+t.root.left.left=Node(4)
+t.root.left.right=Node(5)
+t.root.right.left=Node(6)
 t.root.right.right=Node(7)
 #print(t.show())     #10 3 2 4 6 5 7
-t.inorder_iter()
+t.max()
