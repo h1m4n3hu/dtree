e36f8f370c869cbfef17c4eb5286b275c1113282
h1m4n3hu <himanshusoni2708@gmail.com>
2020-10-19 08:29:31 +0530

    the tree

--- treques.py
+++ treques.py
@@ -10,6 +10,7 @@ class Tree:
     def __init__(self,root):
         self.root=root
         self.sum=0
+        self.s=0
 
     def pre_traversal(self,start,res):
         if start:
@@ -60,20 +61,19 @@ class Tree:
             print(node.value)
 
     def sum(self):
-        sum=0
+        s=0
         l.append(self.root)
         while l:
             k=l.pop()
-            sum+=k.value
+            s+=k.value
             if k.right:
                 l.append(k.right)
             if k.left:
                 l.append(k.left)
-        return sum
+        return s
 
     def sum_left(self):
-        sum=0
-        k=self.root
+        s=0
         l.append(self.root)
         while len(l)>0:
             node=l.pop()
@@ -81,8 +81,8 @@ class Tree:
                 l.append(node.right)
             if node.left:
                 l.append(node.left)
-                sum+=node.left.value
-        return sum
+                s+=node.left.value
+        return s
 
     def level_travel(self):
         l.append(self.root)
@@ -94,14 +94,51 @@ class Tree:
             elif node.right:l.insert(0,node.right)
         print(res)
 
-    def leaf_sum(self,root):
-        if not root.left and not root.right:
-            self.sum+=root.value
-        if root.left:
-            self.leaf_sum(root.left)
-        if root.right:
-            self.leaf_sum(root.right)
+    def leaf_sum(self,node):
+        if node.left or node.right:
+            if node.left.value==2 or node.right.value==2:
+                self.sum+=node.value
+        if node.left:
+            self.leaf_sum(node.left)
+        if node.right:
+            self.leaf_sum(node.right)
         print(self.sum)
+
+    def longest(self,head):
+        print(head.value)
+        if head.right:
+            self.longest(head.right)
+        if head.left:
+            self.longest(head.left)
+
+    def leaves(self,head):
+        if head:
+            pass
+        if head.left:
+            self.leaves(head.left)
+        else:
+            self.sum+=head.value
+            print(head.value)
+        if head.right:
+            self.leaves(head.right)
+        #return self.sum
+
+    def inorder_list(self,head):
+        if head.left:
+            self.inorder_list(head.left)
+        if head.value:
+            l.append(head)
+        if head.right:
+            self.inorder_list(head.right)
+        return l
+
+    def inorder_successor(self,value):
+        l=self.inorder_list(self.root)
+        j=0
+        for i in l:
+            if i.value==value:
+                print(l[j+1].value)
+            j+=1
 
 t=Tree(Node(1))
 t.root.left=Node(2)
@@ -113,4 +150,4 @@ t.root.right.right=Node(7)
 #t.preorder_iter()   #1 2 4 5 3 6 7
 #t.inorder_iter()    #4 2 5 1 6 3 7
 #t.postorder_iter()  #4 5 2 6 7 3 1
-t.leaf_sum(t.root)
+t.inorder_successor(6)
