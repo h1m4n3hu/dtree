ccc43ba5c193d8f39afa0d6eb9a1ce5d6e93dc16
h1m4n3hu <himanshusoni2708@gmail.com>
2020-11-01 09:03:54 +0530

    the tree

--- dree.py
+++ dree.py
@@ -7,12 +7,34 @@ class Node:
 class Tree:
     def __init__(self):
         self.head=None
+        self.i=0
 
     def traverse(self,head):
         if head:
+            print(head.value,end=" ")       #head
             self.traverse(head.left)        #left
             self.traverse(head.right)       #right
-            print(head.value,end=" ")       #head
+
+    def height(self,head):
+        if head is None:
+            return 0
+        lheight=self.height(head.left)
+        rheight=self.height(head.right)
+        return 1+max(lheight,rheight)
+
+    def diameter(self,head):
+        if head is None:
+            return 0
+        lheight=self.height(head.left)
+        rheight=self.height(head.right)
+        return 1+lheight+rheight
+
+    def size(self,head):
+        if head:
+            self.i+=1
+            self.size(head.left)        #left
+            self.size(head.right)       #right
+        return self.i
 
 t=Tree()
 t.head=Node(1)
@@ -22,7 +44,7 @@ t.head.left.left=Node(4)
 t.head.left.right=Node(5)
 t.head.right.left=Node(6)
 t.head.right.right=Node(7)
-t.traverse(t.head)
+print(t.size(t.head))
 
     #        1
     #      /  \
