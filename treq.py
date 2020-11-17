534d2b580493f4ebb33d3f2be05ee84035461086
h1m4n3hu <himanshusoni2708@gmail.com>
2020-11-17 17:53:34 +0530

    the tree

--- treq.py
+++ treq.py
@@ -39,14 +39,30 @@ class Tree:
                     pre.right=k
                     k=k.left
                 else:
+                    print(k.value,'i')
                     pre.right=None
-                    print(k.value)
                     k=k.right
             else:
                 print(k.value)
                 k=k.right
 
-
+    def morris_preord(self):
+        k=self.root
+        while k:
+            if k.left:
+                pre=k.left
+                while pre.right and pre.right!=k:
+                    pre=pre.right
+                if pre.right is None:
+                    print(k.value,'i')
+                    pre.right=k
+                    k=k.left
+                else:
+                    pre.right=None
+                    k=k.right
+            else:
+                print(k.value)
+                k=k.right
 
 t=Tree()
 t.root=Node(1)
@@ -54,3 +70,6 @@ t.root.left=Node(2)
 t.root.right=Node(3)
 t.root.left.left=Node(4)
 t.root.left.right=Node(5)
+t.root.right.left=Node(6)
+t.root.right.right=Node(7)
+t.morris_preord()
