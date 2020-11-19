4b59b8302b8b9840c90de715b895e559141411d1
h1m4n3hu <himanshusoni2708@gmail.com>
2020-11-19 14:23:27 +0530

    the tree

--- tresum.py
+++ tresum.py
@@ -15,6 +15,37 @@ class Tree:
             self.traverse(head.left)
             self.traverse(head.right)
 
+    def chilsum(self,head):
+        if head and head.right and head.left:
+            print(head.value==head.right.value+head.left.value)
+            self.chilsum(head.left)
+            self.chilsum(head.right)
+
+    def levelof(self,root,val,lev=1):
+        if root:
+            if root.value==val:
+                self.num+=lev
+            self.levelof(root.left,val,lev+1)
+            self.levelof(root.right,val,lev+1)
+        return self.num
+
+    def unfulltree(self,head):
+        if head:
+            if not head.left and head.right or not head.right and head.left:
+                print(head.value)
+            self.unfulltree(head.left)
+            self.unfulltree(head.right)
+
+    def iterfulltree(self):
+        k=self.root
+        l=[]
+        l.append(k)
+        while l:
+            a=l.pop()
+            if not a.left and a.right or not a.right and a.left:
+                print(a.value)
+            if a.right:l.append(a.right)
+            if a.left:l.append(a.left)
 
 t=Tree()
 t.root=Node(1)
@@ -24,4 +55,4 @@ t.root.left.left=Node(4)
 t.root.left.right=Node(5)
 t.root.right.left=Node(6)
 t.root.right.right=Node(7)
-t.height(t.root)
+t.iterfulltree()
